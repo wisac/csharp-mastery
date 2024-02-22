72a56d94b8c750f4a318956f1ac4c2f00ff051c0
Isaac Wilson <iswilson.dev@gmail.com>
2024-02-22 22:01:23 +0000

    working with dates

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,7 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
 
-var first = 1;
+int? age = null;
+string? name = null;
+
+DateTime today = DateTime.UtcNow;
+
+// get system timezone
+var zone = TimeZoneInfo.Local;
+
+Console.WriteLine(zone);
+
+// format time
+DateTime birthday = DateTime.Parse("12-1-23", CultureInfo.InvariantCulture);
+DateTime tomorrow = DateTime.ParseExact("22-02-24", "dd-M-yy",CultureInfo.InvariantCulture);
+
+Console.WriteLine(tomorrow);
 
 
+// change console background
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("Press any key to start");
+Console.ReadKey();
+Console.WriteLine(today.ToString("\ndd MMMMM,yyyy hh:mm tt zz"));
 
+// clear console 
+Console.Clear();
--- Variables/obj/Debug/net8.0/Variables.AssemblyInfo.cs
+++ Variables/obj/Debug/net8.0/Variables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Variables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc3af658d5b7a2e4ff053b9c9ee01e736c54d5fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Variables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Variables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
