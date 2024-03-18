92aadfe81cee0e6ab48c6a9d56af63bb2e6e1896
Isaac Wilson <iswilson.dev@gmail.com>
2024-03-18 19:24:04 +0000

    files and folders

--- FilesAndDirectories/Program.cs
+++ FilesAndDirectories/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// creating new file
+// File.Create("./test"); // creates test at current dir
+
+// appending content to a file (creates file if non-existent)
+File.AppendAllLines("test", ["Mango", "Oranges"]);
+
+FileAttributes details = File.GetAttributes("test");
+
+Console.WriteLine(details);
+
+// copy file 
+// File.Copy("test", "test3.txt");
+
+// delete file
+File.Delete("./test");
+
+File.SetCreationTime("test3.txt", DateTime.Now);
+
+Console.WriteLine(File.GetAttributes("test3.txt"));
+
+
+// working with directories
+
+Console.WriteLine("Created At {0}", Directory.GetCreationTimeUtc("/home/wilson/Documents/Legon"));
+Console.WriteLine(Directory.GetFiles("/"));
+
+foreach (var item in Directory.GetDirectories("/"))
+{
+   Console.WriteLine(item);
+}
+
+DirectoryInfo dir = new("./dir"); // initialize a new directory instance
+
+dir.Create(); // creates new directory
+Console.WriteLine(dir.Name);
+
+// create symbolic link to folder
+// File.CreateSymbolicLink("dirShort", "dir");
+
+Console.WriteLine("Directory exist dirShort: {0}",Directory.Exists("dirShort"));
+
+// instantiate new instance of a file
+// FileInfo fl = new("file.txt");
+// fl.Create();
+
+// copy file
+File.Copy("file.txt", "dir/fileCopy.txt",true); // copy and overwrite file
+
+Console.WriteLine(Path.DirectorySeparatorChar); // /
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
